138ecdb71169384a5c171658e564c4d5715d5c6f
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-16 12:28:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace uppgift5.polymorfism.sep16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
